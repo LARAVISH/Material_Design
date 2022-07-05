3f050cb4d4949c1cac56b733e828325d59466bac
Lara Vish <fandvor@mail.ru>
2022-07-05 21:10:03 +0500

    добавила темную тему |  применила автоматическую тёмную тему для девайсов на 10+ Android. (работает, но нужно с колором еще поработать) HW 2 the tnd

--- app/src/main/java/com/githab/laravish/material_design/MyApp.kt
+++ app/src/main/java/com/githab/laravish/material_design/MyApp.kt
@@ -1,11 +1,13 @@
 package com.githab.laravish.material_design
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import com.google.android.material.color.DynamicColors
 
 class MyApp : Application() {
     override fun onCreate() {
         super.onCreate()
         DynamicColors.applyToActivitiesIfAvailable(this)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.getDefaultNightMode())
     }
 }
