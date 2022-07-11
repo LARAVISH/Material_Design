b919f03fc7cdff0290dd8e54f86b5f1b212251f7
Lara Vish <fandvor@mail.ru>
2022-07-11 15:55:15 +0500

    добавила behavior для nestedScroll и FAB | переопредилила  цвет FAB в темах

--- app/src/main/java/com/githab/laravish/material_design/ui/main/layouts/CoordinatorLayoutFragment.kt
+++ app/src/main/java/com/githab/laravish/material_design/ui/main/layouts/CoordinatorLayoutFragment.kt
@@ -5,23 +5,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.githab.laravish.material_design.R
+import com.githab.laravish.material_design.databinding.FragmentCoordinatorLayoutBinding
 
 class CoordinatorLayoutFragment : Fragment() {
 
+    private var _binding: FragmentCoordinatorLayoutBinding? = null
+    private val binding: FragmentCoordinatorLayoutBinding
+        get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-
-        return inflater.inflate(R.layout.fragment_coordinator_layout, container, false)
+    ): View {
+        _binding = FragmentCoordinatorLayoutBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     companion object {
-
         @JvmStatic
         fun newInstance() =
             CoordinatorLayoutFragment()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
