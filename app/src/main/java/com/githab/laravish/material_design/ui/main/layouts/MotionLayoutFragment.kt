e690ef7061ed13bc5b32d4dff233ae685ba79340
Lara Vish <fandvor@mail.ru>
2022-07-11 18:40:31 +0500

    added motion layout| finished HW lesson 4

--- app/src/main/java/com/githab/laravish/material_design/ui/main/layouts/MotionLayoutFragment.kt
+++ app/src/main/java/com/githab/laravish/material_design/ui/main/layouts/MotionLayoutFragment.kt
@@ -5,23 +5,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.githab.laravish.material_design.R
+import com.githab.laravish.material_design.databinding.FragmentMotionLayoutBinding
 
 
 class MotionLayoutFragment : Fragment() {
 
+    private var _binding: FragmentMotionLayoutBinding? = null
+    private val binding: FragmentMotionLayoutBinding
+        get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
+    ): View {
 
-        return inflater.inflate(R.layout.fragment_motion_layout, container, false)
+        _binding = FragmentMotionLayoutBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
     companion object {
         @JvmStatic
         fun newInstance() =
             MotionLayoutFragment()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
